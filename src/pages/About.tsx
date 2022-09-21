5c024803e77c9f4bd5b7f4b2a4a1e711623c1dcc
karamazov85 <farkas.adam.mail@gmail.com>
2022-09-21 13:28:54 +0100

    created route structure

---
+++ src/pages/About.tsx
@@ -0,0 +1,5 @@
+const About:React.FC = () => {
+    return <h1>something About</h1>
+}
+
+export default About
---
+++ src/pages/Store.tsx
@@ -0,0 +1,5 @@
+const Store:React.FC = () => {
+    return <h1>something Store</h1>
+}
+
+export default Store
