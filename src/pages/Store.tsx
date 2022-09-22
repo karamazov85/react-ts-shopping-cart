80e6614b92d0b21d7bdb419d291edcf2862deac3
karamazov85 <farkas.adam.mail@gmail.com>
2022-09-22 08:54:45 +0100

    created and styled StoreItem component

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,12 +1,13 @@
 import { Col, Row } from 'react-bootstrap'
 import storeItems from '../data/items.json'
+import { StoreItem } from '../components/StoreItem'
 
 
 const Store:React.FC = () => {
     return (
     <>
         <Row md={2} xs={1} lg={3} className="g-3">
-           {storeItems.map(item => <Col key={item.id}><StoreItem { ...item }/></Col>)}
+           {storeItems.map(item => <Col key={item.id}><StoreItem { ...item }/> </Col>)}
         </Row>
     </>
     )
