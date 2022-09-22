9a20a1de06a56ea5b84fe282681414f97aa634ea
karamazov85 <farkas.adam.mail@gmail.com>
2022-09-22 08:10:11 +0100

    created Storep page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,22 @@
-import { Navbar as NavbarBs, Container, Nav } from 'react-bootstrap';
+import { Navbar as NavbarBs, Container, Nav, Button } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 const Navbar:React.FC = () => {
     return (
-        <NavbarBs className="bg-white shadow-sm mb-3">
+        <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
             <Container>
-                <Nav>
+                <Nav className="me-auto">
                    <Nav.Link as={NavLink} to="/">Home</Nav.Link>
                    <Nav.Link as={NavLink} to="/store">Store</Nav.Link>
                    <Nav.Link as={NavLink} to="/about">About</Nav.Link>
                 </Nav>
+                <Button style={{ width: "3rem", height: "3rem", position: "relative" }} variant="outline-primary" className="rounded-circle" >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+               
+                <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center" style={{ color: "white", width:"1.5rem", height:"1.5rem", position: "absolute", bottom:"0", right:"0", transform:"translate(25%, 25%)" }}>
+                    3
+                </div>
+                </Button>
             </Container>
         </NavbarBs>
     )
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,15 @@
+import { Col, Row } from 'react-bootstrap'
+import storeItems from '../data/items.json'
+
+
 const Store:React.FC = () => {
-    return <h1>something Store</h1>
+    return (
+    <>
+        <Row md={2} xs={1} lg={3} className="g-3">
+           {storeItems.map(item => <Col key={item.id}><StoreItem { ...item }/></Col>)}
+        </Row>
+    </>
+    )
 }
 
 export default Store
