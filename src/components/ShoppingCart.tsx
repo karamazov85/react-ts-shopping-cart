32e3eaa39c84ec54d198077d5e0652a9973a8c77
karamazov85 <farkas.adam.mail@gmail.com>
2022-09-24 18:14:51 +0100

    finished ShoppingCart and CartItem components

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,5 +1,8 @@
 import { Offcanvas, OffcanvasHeader, OffcanvasBody, Stack } from "react-bootstrap"
 import { useShoppingCart } from "../context/ShoppingCartContext"
+import { formatCurrency } from "../utilities/formatCurrency";
+import { CartItem } from "./CartItem";
+import storeItems from '../data/items.json'
 
 interface ShoppingCartProps {
     isOpen: boolean;
@@ -16,11 +19,20 @@ export const ShoppingCart:React.FC<ShoppingCartProps> = ({ isOpen }) => {
                 </Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-                <Stack>
+                <Stack gap={2}>
                     {cartItems.map(item => 
                         <CartItem key={item.id} { ...item } />
                     )}
                 </Stack>
+                <div className="ms-auto fw-bold fs-5">
+                    Total: 
+                    {formatCurrency(cartItems.reduce((total, cartItem) => {
+                        
+                        const item = storeItems.find(i => i.id === cartItem.id)
+                        
+                        return total + (item?.price || 0) * cartItem.quantity
+                    },0))}
+                </div>
             </Offcanvas.Body>
         </Offcanvas>
     )
